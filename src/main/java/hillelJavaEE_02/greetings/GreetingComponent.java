a40e7f69b6838367bd96d9c89f1fde1d9b7aa276
venger-alex <venger.alex@gmail.com>
2017-12-07 15:54:24 +0200

    Link the controller and the random greeting component using dependency-injection

--- src/main/java/hillelJavaEE_02/greetings/GreetingComponent.java
+++ src/main/java/hillelJavaEE_02/greetings/GreetingComponent.java
@@ -1,13 +1,15 @@
 package hillelJavaEE_02.greetings;
 
 import lombok.Data;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 @Data
-public class greetingComponent {
+@Component
+public class GreetingComponent {
     private List<String> greetingsList = new ArrayList<String>() {{add("Hello world!");
         add("Hola world!");
         add("Bonjour world!");}};
