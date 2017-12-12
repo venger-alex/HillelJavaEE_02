76a0368da91cbce855a9e3e9902cd0a54d50a4af
venger-alex <venger.alex@gmail.com>
2017-12-12 14:07:49 +0200

    RequestParam and Optional

--- src/main/java/hillelJavaEE_02/pet/PetController.java
+++ src/main/java/hillelJavaEE_02/pet/PetController.java
@@ -1,15 +1,46 @@
 package hillelJavaEE_02.pet;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 @RestController
 public class PetController {
+    private List<Pet> pets = new ArrayList<Pet>() {{
+        add(new Pet("Tom", "Cat", 3));
+        add(new Pet("Jerry", "Mouse", 1));
+    }};
+
     //@RequestMapping(value = "/greeting", method = RequestMethod.GET)
     @GetMapping("/greeting")
     public String helloWorld() {
         return "Hello world!";
     }
+
+    @GetMapping("/pets")
+    public List<Pet> getPets(@RequestParam Optional<String> species) {
+        Predicate<Pet> speciesFilter = species.map(this::filterBySpecies).orElse(pet -> true);
+
+        return pets.stream()
+                .filter(speciesFilter)
+                .collect(Collectors.toList());
+    }
+
+    private Predicate<Pet> filterBySpecies(String species) {
+        return pet -> pet.getSpecies().equals(species);
+    }
+}
+
+@Data
+@AllArgsConstructor
+class Pet {
+    private String name;
+    private String species;
+    private Integer age;
 }
