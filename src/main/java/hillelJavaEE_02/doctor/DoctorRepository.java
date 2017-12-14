75fa0e9e0542c05ed709882dc1d9e04478b381bf
venger-alex <venger.alex@gmail.com>
2017-12-14 23:32:35 +0200

    Add verification: when trying to create (or modify) a doctor, his specialty should be from the list in the configuration

--- src/main/java/hillelJavaEE_02/doctor/DoctorRepository.java
+++ src/main/java/hillelJavaEE_02/doctor/DoctorRepository.java
@@ -1,5 +1,6 @@
 package hillelJavaEE_02.doctor;
 
+import hillelJavaEE_02.doctor.util.NoSuchSpecializationException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -33,6 +34,10 @@ public class DoctorRepository {
     }
 
     public Doctor save(Doctor doctor) {
+        if(!this.findAllSpecialization().contains(doctor.getSpecialization())) {
+            throw new NoSuchSpecializationException("The specialty should be from the list in the configuration: " + doctor.getSpecialization());
+        }
+
         if (doctor.getId() == null) {
             doctor.setId(ThreadLocalRandom.current().nextInt(0, Integer.MAX_VALUE));
         }
