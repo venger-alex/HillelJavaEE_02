1124e594e6869f7839c309ea6c098bfe6f4c92c4
venger-alex <venger.alex@gmail.com>
2017-12-14 16:59:20 +0200

    config

--- src/main/java/hillelJavaEE_02/clinic/ClinicController.java
+++ src/main/java/hillelJavaEE_02/clinic/ClinicController.java
@@ -2,30 +2,26 @@ package hillelJavaEE_02.clinic;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class ClinicController {
 
-    private String clinicName;
-    private String workingHours;
-
-    public ClinicController(@Value("${clinic.name}") String clinicName,
-                            @Value("${clinic.working-hours}") String workingHours) {
-        this.clinicName = clinicName;
-        this.workingHours = workingHours;
-    }
+    private final ClinicInfo clinicInfo;
 
     @GetMapping("/clinic/info")
     public ClinicInfo getClinicInfo() {
-        return new ClinicInfo(clinicName, workingHours);
+        return clinicInfo;
     }
 }
 
 @Data
-@AllArgsConstructor
+@Component
+@ConfigurationProperties("clinic-info")
 class ClinicInfo {
     private String name;
     private String workingHours;
